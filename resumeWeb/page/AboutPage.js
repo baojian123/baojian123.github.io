b2114f3e5c268f9cf2625a9c7a837ae97b5912c2
baojian123 <q351211168@gmail.com>
2021-01-11 00:23:07 +1100

    Add comming soon at About Page

--- resumeWeb/page/AboutPage.js
+++ resumeWeb/page/AboutPage.js
@@ -3,9 +3,19 @@
 const AboutPage = (props) => {
   return (
     <section id={props.pageId} class="page">
-    <div class="page__header">
-      <h2>About</h2>
-    </div>
+      <div class="page__header">
+        <h2>About</h2>
+      </div>
+      <div class="page__content">
+        <div class="info-temp">
+          <h3>Coming Soon</h3>
+          <div class="ellipsis">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
